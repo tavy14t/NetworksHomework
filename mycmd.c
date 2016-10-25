58cc3336f47c223843940023fa60360cb4b29d45
Ilie Octavian <tavy14t@gmail.com>
2016-10-25 19:04:33 +0300

    adaugat parser de argumente si sistem de debugging

--- mycmd.c
+++ mycmd.c
@@ -7,16 +7,29 @@
 #include <fcntl.h>
 #include <dirent.h>
 #include <errno.h>
+#include <ctype.h>
 #include "sha256.h"
 
+#define INPUT_LEN 276
+#define ARG_LEN 256
+#define CMD_LEN 20
 //#define GENERATE_SHA_KEY
+#define DEBUG
 
 void PrintHelp()
 {
 	printf("login         - pentru logarea in aplicatie\n");
 	printf("myfind <file> - cautare fisier recursiv\n");
 	printf("mystat <file> - afiseaza atribute fisiier\n");
-	printf("quit          - inchide aplicatia\n");
+	printf("quit / exit   - inchide aplicatia\n");
+}
+
+char* GetHexSHA(char* sha)
+{
+	char *hex_buffer = (char*) calloc(65, 0);
+	for (int i=0; i<SHA256_BLOCK_SIZE; i++)
+		sprintf(hex_buffer, "%s%02X", hex_buffer, sha[i] & 0xff);
+	return hex_buffer;
 }
 
 int CheckPasswordHash(char pass[31])
@@ -40,6 +53,11 @@ int CheckPasswordHash(char pass[31])
 	fscanf(fp, "%s", &sha2);
 	sha2[SHA256_BLOCK_SIZE] = '\0';
 
+#ifdef DEBUG
+	printf("[DEBUG] SHA@keyboard: %s\n", GetHexSHA(sha1));
+	printf("[DEBUG] SHA@file    : %s\n", GetHexSHA(sha2));
+#endif
+
 	return memcmp(sha1, sha2, SHA256_BLOCK_SIZE);
 }
 
@@ -49,7 +67,8 @@ void LogIn()
 	int login_pipe[2], login_pipe_ret[2];
 
 	printf("Introdu parola: ");
-	scanf("%30s[^\n]", &pass);
+	fgets(pass, 30, stdin);
+	( (char*) strchr(pass, '\n') )[0] = '\0';
 	
 	if(pipe(login_pipe) == -1){
 		fprintf(stderr, "Eroare la pipe-ul de login.\n");
@@ -84,9 +103,89 @@ void LogIn()
 	}
 }
 
+void ParseString(char* input, char *name, char *arg1)
+{
+	char input_copy[INPUT_LEN];
+
+	memset(name, 0, CMD_LEN);
+	memset(arg1, 0, ARG_LEN);
+	memset(input_copy, 0, INPUT_LEN);
+
+	strncpy(input_copy, input, strlen(input));
+
+	int index = 0;
+	int input_copyLen = strlen(input_copy);
+	while (input_copy[index] == ' ') index++;
+	for (int i = 0; i < input_copyLen - index; i++)
+		input_copy[i] = input_copy[i + index];
+	input_copy[input_copyLen - index] = '\0';
+
+	input_copyLen = strlen(input_copy);
+	for (int i = 0; i <= input_copyLen; i++)
+		if (input_copy[i] == ' ' || input_copy[i] == '\0')
+		{
+			strncpy(name, input_copy, i);
+			index = i;
+			name[i] = '\0';
+			break;
+		}
+
+	if (input_copy[index + 1] == '"')
+	{
+		index += 2;
+		int startIndex = index;
+		while (input_copy[index] != '"')
+			index++;
+
+		for (int i = startIndex; i < index; i++)
+			arg1[i - startIndex] = input_copy[i];
+		arg1[index - startIndex] = '\0';
+		index++;
+	}
+	else if (input_copy[index] == ' ')
+	{
+		int startIndex = index + 1;
+		index++;
+		while (input_copy[index] != ' ' && input_copy[index] != '\0') index++;
+
+		for (int i = startIndex; i < index; i++)
+			arg1[i - startIndex] = input_copy[i];
+		arg1[index - startIndex] = '\0';
+	}
+}
 
 int main(int argc, char** argv)
 {
+	char input[INPUT_LEN];
+	char command[CMD_LEN];
+	char arg[ARG_LEN];
+	
 	LogIn();
-	printf("E bine!");
+
+	while(1)
+	{
+		fgets(input, 276, stdin);
+		( (char*) strchr(input, '\n') )[0] = '\0';
+
+		ParseString(input, command, arg);
+
+#ifdef DEBUG
+		printf("[DEBUG] input   : %s\n", input);
+		printf("[DEBUG] command : %s\n", command);
+		printf("[DEBUG] argument: %s\n", arg);
+#endif
+
+		if(!strcmp(command, "help"))
+			PrintHelp();
+		else if(!strcmp(command, "quit") || !strcmp(command, "exit"))
+			exit(0);
+		else if(!strcmp(command, "login"))
+			LogIn();
+		else if(!strcmp(command, "myfind")){
+	
+		}
+		else if(!strcmp(command, "mystat")){
+
+		}
+	}
 }
